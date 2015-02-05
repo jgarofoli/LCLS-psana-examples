261ebbc9d1c8c3842b7aa022ef9bcbe99139bff4
Justin Garofoli <justing@slac.stanford.edu>
2015-02-04 17:33:33 -0800

    stop that huge log file from growing

--- data_summary_tools/data_summary/__init__.py
+++ data_summary_tools/data_summary/__init__.py
@@ -9,26 +9,26 @@ from device_config import *
 logger = logging.getLogger('data_summary')
 logger.setLevel(logging.DEBUG)
 
-fh = logging.FileHandler('data_summary.log')
-fh.setLevel(logging.DEBUG)
+#fh = logging.FileHandler('data_summary.log')
+#fh.setLevel(logging.DEBUG)
 
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
-fh.setFormatter(formatter)
+#fh.setFormatter(formatter)
 ch.setFormatter(formatter)
 
-logger.addHandler(fh)
+#logger.addHandler(fh)
 logger.addHandler(ch)
 
 
 def set_logger_level(lvl):
     logger.setLevel( getattr(logging,lvl) )
-    fh.setLevel( getattr(logging,lvl) )
+#    fh.setLevel( getattr(logging,lvl) )
     ch.setLevel( getattr(logging,lvl) )
     return
 
 def logger_flush():
-    fh.flush()
+#    fh.flush()
     return
